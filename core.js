8aa3c6248f70539089ef42fa85e5be0b25c55c54
Patty-Chen <1093832480@qq.com>
2017-09-11 18:06:44 +0800

    [bugfix] 1 如果onResolved无法执行 需要继续直接resolved返回的promise          2 当调用then时会有两个promise 需要设置第一个promise的回调          3 pending状态下 then调用时 此时没有value或reason 回调函数不能直接使用此时的值

--- core.js
+++ core.js
@@ -1,89 +1,194 @@
+const asyncCall = function(callee) {
+  if (typeof (callee) !== 'function') {return}
+  if (typeof (process) === 'object' && process !== null && typeof(process.nextTick) === 'function'){
+    process.nextTick(callee)
+  }
+  if (typeof (setImmediate) === 'function'){
+    setImmediate(callee)
+  }
+  else{
+    setTimeout(callee,0)
+  }
+}
 
 const enumState = {PENDING:'pending',FULFILLED:'fulfilled',REJECTED:'rejected'}
 const enumEvent = {RESOLVE:'resolve',REJECT:'reject'}
-console.log(enumState)
 
 function myPromise(executor) {
   this.state = enumState.PENDING
   this.data = undefined
-  this.onResolvedCallbacks = []
-  this.onRejectedCallbacks = []
+  this.onResolved = undefined
+  this.onRejected = undefined
+
   this.resolve = (value) => {
-    runPromiseFSM(this, enumEvent.resolve, value)
+    runPromiseFSM(this, enumEvent.RESOLVE, value)
   }
 
   this.reject = (reason) => {
-    runPromiseFSM(this, enumEvent.reject, reason)
+    runPromiseFSM(this, enumEvent.REJECT, reason)
   }
 
   executor(this.resolve, this.reject)
+
 }
 
 function runPromiseFSM(srcPromise, event, data){
-    if (srcPromise.state !== enumState.PENDING){return}
+  if (srcPromise.state !== enumState.PENDING){return}
 
-    if (event === enumEvent.RESOLVE && srcPromise.onResolved){
-        srcPromise.onResolved(data)
-    }
-    else if (event === enumEvent.RESOLVE && !srcPromise.onResolved){
-        this.data = data
-    }
-    else if (event === enumEvent.REJECT && srcPromise.onRejected){
-        srcPromise.onRejected(data)
-    }
-    else if (event === enumEvent.REJECT && !srcPromise.onRejected){
-        this.data = data
-    }
+  if (event === enumEvent.RESOLVE && srcPromise.onResolved){
+    srcPromise.state = enumState.FULFILLED
+    srcPromise.onResolved(data)
+
+    //asyncCall(srcPromise.onResolved.bind(null, data))
+  }
+  else if (event === enumEvent.RESOLVE && !srcPromise.onResolved){
+    srcPromise.state = enumState.FULFILLED
+    srcPromise.data = data
+  }
+  else if (event === enumEvent.REJECT && srcPromise.onRejected){
+    srcPromise.state = enumState.REJECTED
+    srcPromise.onRejected(data)
+    //asyncCall(srcPromise.onRejected.bind(null, data))
+  }
+  else if (event === enumEvent.REJECT && !srcPromise.onRejected){
+    srcPromise.state = enumState.REJECTED
+    srcPromise.data = data
+  }
 }
 
 myPromise.prototype.then = function(onResolved,onRejected){
-    let data = this.data
-    onResolved = typeof(onResolved) === 'function' ? onResolved : null
-    onRejected = typeof(onRejected) === 'function' ? onRejected : null
+  let self = this
+  let data = this.data
 
-    if (this.state === enumState.PENDING){
-        return new myPromise(function(resolve,reject){
-            this.onResolved = function(){
-                let x = asyncCall(onResolved(data))
-                if (x instanceof myPromise){
-                    x.then(resolve,reject)
-                }
-                else{
-                    resolve(x)
-                }
-            }
-            this.onRejected = function(){
-                let x = onRejected(data)
-                if(x instanceof myPromise){
-                    x.then(resolve,reject)
-                }
-                else{
-                    resolve(x)
-                }
-            }
-            
-        })
-    }
-    else if (this.state === enumState.FULFILLED){
-        return new myPromise(function(resolve,reject){
-            let x = onResolved(data)
-            if (x instanceof myPromise){
-                x.then(resolve,reject)
-            }
-            else{
-                resolve(x)
-            }
-        })
-    }
-    else if (this.state === enumState.REJECTED){
-         return new myPromise(function(resolve,reject){
-            let x = onRejected(data)
-            if (x instanceof myPromise){
-                x.then(resolve,reject)
-            }
-            else{
-                resolve(x)            
-            }
-        })
-    }
+  onResolved = typeof(onResolved) === 'function' ? onResolved : null
+  onRejected = typeof(onRejected) === 'function' ? onRejected : null
+
+  if (this.state === enumState.PENDING){
+    return new myPromise(function(resolve,reject){
+      self.onResolved = function(value){
+        try{
+          let x
+          if (onResolved){
+            //x = asyncCall(onResolved.bind(null, data))
+            x = onResolved(value)
+          }
+          else{
+            resolve(value)
+            return
+          }
+          if (x === self){
+            throw new TypeError('then is not allowed to return the promise itself')
+          }
+          if (x instanceof myPromise){
+            x.then(resolve,reject)
+          }
+          else{
+            resolve(x)
+          }
+        }
+        catch(exception){
+          reject(exception)
+        }
+      }
+      self.onRejected = function(reason){
+        try{
+          let x
+          if (onRejected) {
+            //x = asynCall(onRejected.bind(null, data))
+            x = onRejected(reason)
+          }
+          else{
+            reject(reason)
+            return
+          }
+          if (x === self){
+            throw new TypeError('then is not allowed to return the promise itself')
+          }
+          if(x instanceof myPromise){
+            x.then(resolve,reject)
+          }
+          else{
+            resolve(x)
+          }
+        }
+        catch(exception){
+          reject(exception)
+        }
+      }
+    })
+  }
+  else if (this.state === enumState.FULFILLED){
+    console.log('FULFILLED')
+    return new myPromise(function(resolve,reject){
+      try{
+        let x
+        if (onResolved){
+          //x = asyncCall(onResolved.bind(null, data))
+          x = onResolved(data)
+        }
+        else{
+          resolve(data)
+          return
+        }
+        console.log(x)
+        if (x === self){
+          throw new TypeError('then is not allowed to return the promise itself')
+        }
+        if (x instanceof myPromise){
+          x.then(resolve,reject)
+        }
+        else{
+          resolve(x)
+        }
+      }
+      catch(exception){
+        comsole.log(exception)
+        reject(exception)
+      }
+    })
+  }
+  else if (this.state === enumState.REJECTED){
+    console.log('rejected')
+    return new myPromise(function(resolve,reject){
+      try{
+        let x
+        if (onRejected){
+          //x = asyncCall(onRejected.bind(null, data))
+          x = onRejected(data)
+        }
+        else{
+          reject(data)
+          return
+        }
+        if (x === self){
+          throw new TypeError('then is not allowed to return the promise itself')
+        }
+        if (x instanceof myPromise){
+          x.then(resolve,reject)
+        }
+        else{
+          resolve(x)
+        }
+      }
+      catch(exception){
+        reject(exception)
+      }
+    })
+  }
 }
+
+adapter = {
+  deferred : function() {
+    let dfd = {};
+    dfd.promise = new myPromise((resolve, reject) => {
+      dfd.resolve = resolve;
+    dfd.reject = reject;
+  });
+    return dfd;
+  }
+}
+
+if (module && 'exports' in module) {
+  module.exports = adapter;
+}
+
